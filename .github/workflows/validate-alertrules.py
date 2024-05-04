562061494ebbba4074d415473d1e7975e7a65584
gokulyadav21 <124569852+gokulyadav21@users.noreply.github.com>
2024-05-04 12:10:09 +0530

    Update validate-alertrules.py

--- .github/workflows/validate-alertrules.py
+++ .github/workflows/validate-alertrules.py
@@ -1,5 +1,6 @@
 import sys
 import sqlvalidator
+import subprocess
 
 def extract_query_lines(file_path):
     try:
@@ -11,6 +12,45 @@ def extract_query_lines(file_path):
         print(f"Error reading file {file_path}: {e}")
         return []
 
+def extract_query(file_path):
+    grep_command = ["grep", "^ *query:", file_path]
+    awk_command = ["awk", '{$1=""; print $0}']
+    
+    try:
+        grep_process = subprocess.Popen(grep_command, stdout=subprocess.PIPE)
+        awk_process = subprocess.Popen(awk_command, stdin=grep_process.stdout, stdout=subprocess.PIPE)
+        grep_process.stdout.close()  # Close grep stdout to allow awk to finish
+        
+        output, _ = awk_process.communicate()
+        return output.decode().splitlines()
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return []
+
+def validate_query(query, idx):
+    errors = []
+
+    # Check if there are equal number of opening and closing brackets
+    if query.count('(') != query.count(')'):
+        errors.append("Unbalanced parentheses")
+
+    # Check if there are equal number of single quotes
+    if query.count("'") % 2 != 0:
+        errors.append("Unbalanced single quotes")
+
+    # Check if there are equal number of double quotes
+    if query.count('"') % 2 != 0:
+        errors.append("Unbalanced double quotes")
+
+    if errors:
+        print(f"Query line {idx} is invalid: {query}")
+        for error in errors:
+            print(f"    - {error}")
+        return False
+    else:
+        print(f"Query line {idx} is valid: {query}")
+        return True
+    
 def format_and_validate_query(query, idx):
     query = query.replace('"', "'")
     query = f"SELECT {query} FROM table"
@@ -35,8 +75,14 @@ invalid_queries = []
 
 for file_path in file_paths:
     query_lines = extract_query_lines(file_path)
+    queryies = extract_query(file_path)
     for idx, query in enumerate(query_lines, start=1):
         if not format_and_validate_query(query, idx):
             sys.exit(1)
-
-sys.exit(0)
+    for idx, line in enumerate(queryies, start=1):
+        if not validate_query(line, idx):
+            invalid_queries.append(line)
+    if invalid_queries:
+        sys.exit(1)
+    else:
+        sys.exit(0)
