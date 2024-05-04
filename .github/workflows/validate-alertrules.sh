c1f9c6535c8b0ebadf7eb5b9f226e581034660a9
gokulyadav21 <124569852+gokulyadav21@users.noreply.github.com>
2024-05-04 12:01:38 +0530

    Create validate-alertrules.sh

---
+++ .github/workflows/validate-alertrules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a Bash script that runs a Python script
+
+# Change directory to the location of the Python script
+cd .github/workflows/
+
+# Run the Python script
+python validate-alertrules.py
